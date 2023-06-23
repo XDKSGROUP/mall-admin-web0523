22e08828972da2f1a79e5f4b9bc28e0e48236fae
dll <7457322@qq.com>
2023-06-24 01:29:57 +0800

    提现列表

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -1,9 +1,29 @@
-export const enumMemberAuthStatus=[{
-  name:"未实名",value:"0"
-},{
-  name:"已申请",value:"1"
-},{
-  name:"已驳回",value:"2"
-},{
-  name:"已通过",value:"3"
+export const enumMemberAuthStatus = [{
+  name: "未实名", value: "0"
+}, {
+  name: "已申请", value: "1"
+}, {
+  name: "已驳回", value: "2"
+}, {
+  name: "已通过", value: "3"
 }];
+export const enumMoneyType = [{
+  name: "爱心值", value: "0"
+}, {
+  name: "贡献值", value: "1"
+}];
+export const enumWithdrawalsStatus = [{
+  name: "未打款", value: "0"
+}, {
+  name: "已打款", value: "1"
+}, {
+  name: "已作废", value: "2"
+}];
+export const enumAuthStatus = [{
+  name: "已申请", value: "0"
+}, {
+  name: "已通过", value: "1"
+}, {
+  name: "已驳回", value: "2"
+}];
+
