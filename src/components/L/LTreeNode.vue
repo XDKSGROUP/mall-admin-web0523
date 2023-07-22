d6212c6457784c8609e71ffaaf6e624b01370b7d
dll <7457322@qq.com>
2023-07-23 03:28:15 +0800

    更新

--- src/components/L/LTreeNode.vue
+++ src/components/L/LTreeNode.vue
@@ -4,8 +4,16 @@
             <div :class="index == 0 ? 'linel' : index == max ? 'liner' : 'line'" v-if="level != 0">
                 <div class="line"></div>
             </div>
-            <div class="title">{{ item.title }}</div>
-            <nodes v-if="item.children" v-model="item.children" :level="lv"></nodes>
+            <div class="title" :class="item[keyTitle] == current ? 'current' : ''">
+                <div @click="handelSelect(item, index)">{{ item[keyTitle] }}</div>
+                <div v-if="!isLoad[index]" class="icon" @click="handelShowHide(item, index)">
+                    {{ show[index] ? '-' : '+' }}
+                </div>
+            </div>
+            <nodes v-if="show[index] && item.children" v-model="item.children" :current="current" :keyTitle="keyTitle"
+                :level="lv" @next="$emit('next', arguments[0], arguments[1])"
+                @select="$emit('select', arguments[0], arguments[1])">
+            </nodes>
         </div>
     </div>
 </template>
@@ -22,19 +30,59 @@ export default {
     props: {
         level: Number,
         children: Array,
+        keyTitle: {
+            type: String,
+            default: "title"
+        },
+        current: String
     },
     data() {
         return {
+            show: undefined,
+            index: undefined,
             max: undefined,
-            lv: undefined
+            lv: undefined,
+            select: undefined,
+            isLoad: [],
         }
     },
     created() {
-        this.max = this.children.length - 1;
-        this.lv = this.level + 1;
+        const me = this;
+        me.lv = me.level + 1;
+        me.max = me.children && (me.children.length - 1) || 0;
+        me.show = me.children.map(t => t.children ? true : false);
+        me.isLoad = me.children.map(t => t.isChild ? false : true);
     },
     methods: {
-
+        //展示和隐藏切换
+        handelShowHide(item, index) {
+            const me = this, s = me.show;
+            if (!s[index] && item.isChild && !item.children) me.handelNext(item, index);
+            s[index] = !s[index];
+            me.show = s.map(t => t);
+        },
+        //选择
+        handelSelect(item, index) {
+            this.index = index;
+            this.select = item;
+            this.$emit("select", item, this);
+        },
+        //读取下1个
+        handelNext(item, index) {
+            this.index = index;
+            this.select = item;
+            this.$emit("next", item, this);
+        },
+        //重新加载
+        reload() {
+            const me = this, s = me.show;
+            me.select.isChild = me.select.children && me.select.children.length;
+            me.isLoad[me.index] = me.select.isChild ? false : true;
+            s[me.index] = false;
+            me.show = s.map(t => t);
+            s[me.index] = true;
+            me.show = s.map(t => t);
+        }
     }
 }
 </script>
@@ -96,6 +144,23 @@ export default {
 .title {
     padding: 10px 3px 5px;
     text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.title .icon {
+    width: 10px;
+    height: 10px;
+    line-height: 6px;
+    margin: 0 auto;
+    text-align: center;
+    border: 1px solid #333;
+    border-radius: 50%;
+}
+
+.current {
+    color: #ff0000;
 }
 </style>
   
--- src/views/mmb/teamlist/index.vue
+++ src/views/mmb/teamlist/index.vue
@@ -1,9 +1,65 @@
 <template>
   <div class="container">
-    <nodes v-model="testData" :level="0"></nodes>
+    <el-card class="search" shadow="never" :class="isShowSearchAll ? '' : 'hide'">
+      <div class="title">
+        <div class="l">
+          <i class="el-icon-search"></i>
+          <span>筛选搜索</span>
+        </div>
+        <div class="r">
+          <el-button style="" type="primary" @click="handleSearchList()" size="small">
+            查询搜索
+          </el-button>
+          <el-button style="" @click="handleResetSearch()" size="small">
+            重置
+          </el-button>
+          <el-button style="" @click="isShowSearchAll = !isShowSearchAll" size="small">
+            {{ isShowSearchAll ? "收缩" : "全部" }}
+          </el-button>
+        </div>
+      </div>
+      <div class="cont">
+        <el-form :inline="true" :model="filter" size="small" label-width="140px">
+          <el-form-item label="用户名：">
+            <el-input v-model="filter.username" class="input-width" placeholder="请输入" clearable></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
+    <nodes v-if="isShowData" v-model="data" keyTitle="username" :current="filter.username" :level="0"
+      @select="handelSelect" @next="handelNext"></nodes>
+    <el-drawer title="详细信息" :visible.sync="showDetail" direction="rtl">
+      <div class="memberinfo">
+        <div class="li">
+          <div class="f">编号：</div><div class="v">{{ selectItem.memberId }}</div>
+        </div>
+        <div class="li">
+          <div class="f">用户名：</div><div class="v">{{ selectItem.username }}</div>
+        </div>
+        <div class="li">
+          <div class="f">电话：</div><div class="v">{{ selectItem.phone }}</div>
+        </div>
+        <div class="li">
+          <div class="f">随机编号：</div><div class="v">{{ selectItem.teamNumber }}</div>
+        </div>
+        <div class="li">
+          <div class="f">团队编号：</div><div class="v">{{ selectItem.commonTeamNumber }}</div>
+        </div>
+        <div class="li">
+          <div class="f">会员角色：</div><div class="v">{{ selectItem.memberLevelName }}</div>
+        </div>
+        <div class="li">
+          <div class="f">会员等级：</div><div class="v">{{ selectItem.memberHonorLevelName }}</div>
+        </div>
+        <div class="li">
+          <div class="f">团队角色：</div><div class="v">{{ selectItem.manageLevelName }}</div>
+        </div>
+      </div>
+    </el-drawer>
   </div>
 </template>
 <script>
+import { listInfo, listInfoNext } from '@/api/team';
 import nodes from '@/components/L/LTreeNode.vue'
 
 export default {
@@ -13,6 +69,14 @@ export default {
   },
   data() {
     return {
+      isShowData: false,
+      data: [],
+      isShowSearchAll: false,
+      filter: {
+        username: ""
+      },
+      selectItem: {},
+      showDetail: false,
       testData: [
         {
           title: '梁当',
@@ -56,14 +120,56 @@ export default {
       ]
     }
   },
-  created() {
+  async created() {
 
   },
   filters: {
 
   },
   methods: {
-
+    async loadData() {
+      const me = this, filter = me.filter;
+      if (!filter.username) { me.data = []; return; }
+      me.isShowData = false;
+      const rst = await listInfo(filter.username);
+      if (rst.code !== 200) this.$message({ type: 'error', message: rst.message });
+      const dic = {};
+      const root = rst.data[0];
+      rst.data.forEach(t => {
+        const code = t.binaryStorage, parent = dic[code.replace(/.$/, "")];
+        t.isChild = t.pointMemberCount ? true : false;
+        dic[code] = t;
+        if (!parent) return;
+        parent.children ? parent.children.push(t) : (parent.children = [t]);
+      });
+      me.data = [root];
+      me.isShowData = true;
+    },
+    async loadNext(item) {
+      if (item.children && item.children.length == 2) return;
+      const me = this, rst = await listInfoNext(item.username);
+      if (rst.code !== 200) me.$message({ type: 'error', message: rst.message });
+      if (!item.children) item.children = [];
+      const dic = {};
+      item.children.forEach(t => dic[t.binaryStorage] = t);
+      rst.data.forEach(t => dic[t.binaryStorage] || (item.children.push(t)));
+    },
+    async handleSearchList() {
+      this.loadData();
+    },
+    async handleResetSearch() {
+      this.filter = {};
+      this.loadData();
+    },
+    async handelSelect(item) {
+      this.selectItem = item;
+      this.showDetail = true;
+      console.log(item)
+    },
+    async handelNext(item, vue) {
+      await this.loadNext(item);
+      vue.reload(item);
+    }
   }
 }
 </script>
@@ -71,6 +177,38 @@ export default {
 .container {
   width: 100%;
 }
+
+.search {
+  height: auto;
+  overflow: hidden;
+  transition: height 0.5s ease-in-out;
+}
+
+.search.hide {
+  height: 118px;
+}
+
+.search .title {
+  overflow: hidden;
+  display: flex;
+  justify-content: space-between;
+}
+
+.search>>>.el-form-item {
+  width: 400px;
+  margin: 15px 0 0;
+}
+
+.search>>>.el-date-editor {
+  width: 250px;
+  padding: 3px 5px;
+}
+
+.search>>>.el-range-separator {
+  width: 20px !important;
+}
+
+
 
 .li {
   width: 50%;
@@ -81,6 +219,29 @@ export default {
 .li .btn {
   cursor: pointer;
 
+}
+
+.memberinfo {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.memberinfo .li {
+  width: 100%;
+  line-height:30px;
+  padding:2px 20px;
+  text-align: left;
+  display: flex;
+}
+.memberinfo .li:nth-child(2n){
+  background-color: #eee;
+}
+.memberinfo .f {
+  width: 120px;
+  text-align: left;
+}
+.memberinfo .v {
+  text-align: left;
 }
 </style>
 
